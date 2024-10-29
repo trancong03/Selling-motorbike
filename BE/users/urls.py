d6c2026d73f2ed7e1baa14930e1d0486b34c2206
Tran Chi Cong <tranchicong.dev@gmail.com>
2024-10-29 22:44:39 +0700

    Modfied project 3 player

--- BE/users/urls.py
+++ BE/users/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path
-from . import views
+from users.views import user
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
-    path('users/<str:id>/', views.update_user, name='update_user'),
-    path('update-images/', views.update_user_images, name='update_user_images'),
-    path('reset-password/', views.reset_password, name='reset_password'),
-    path('reset-password-forgot/', views.reset_password_forgot, name='reset_password_forgot'),
-    path('forgot-password/', views.forgot_password, name='forgot_password'),  
-    path('verify-otp/', views.verify_otp, name='verify_otp'),  
+    path('login/', user.login, name='login'),
+    path('users/<str:id>/', user.update_user, name='update_user'),
+    path('update-images/', user.update_user_images, name='update_user_images'),
+    path('reset-password/', user.reset_password, name='reset_password'),
+    path('reset-password-forgot/', user.reset_password_forgot, name='reset_password_forgot'),
+    path('forgot-password/', user.forgot_password, name='forgot_password'),  
+    path('verify-otp/', user.verify_otp, name='verify_otp'),  
     path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
